8bc16a806538ddc00e4d89d5345e5d9f3de9d0ca
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-11-25 00:53:52 +0300

    feat: add logo, and colors

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const body = document.body;
 const fragment = new DocumentFragment();
 const header = document.createElement('header');
 header.classList.add('header');
-header.innerText='jfdklsjgkfdhgk';
 fragment.append(header);
 
 const contentWrapper = document.createElement('div');
 contentWrapper.classList.add('content-wrapper');
 header.appendChild(contentWrapper);
+
 const headerWrapper = document.createElement('div');
 headerWrapper.classList.add('header-wrapper');
-
+contentWrapper.appendChild(headerWrapper);
+const logoDiv = document.createElement('div');
+headerWrapper.appendChild(logoDiv);
+const linkLogoDiv = document.createElement('a');
+linkLogoDiv.href = "#";
+logoDiv.appendChild(linkLogoDiv);
+const imgLogo = document.createElement('img');
+imgLogo.src='../assets/icons/book.svg';
+imgLogo.alt = "logo";
+linkLogoDiv.appendChild(imgLogo)
 
 body.appendChild(fragment)
+
