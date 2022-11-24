4ede738bc72b3da4a7f16915c4bbbd397724ab5b
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-11-25 01:27:13 +0300

    feat: add icon bag, change header style, add main

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const headerWrapper = document.createElement('div');
 headerWrapper.classList.add('header-wrapper');
 contentWrapper.appendChild(headerWrapper);
 const logoDiv = document.createElement('div');
+logoDiv.style.marginRight = '15px';
 headerWrapper.appendChild(logoDiv);
 const linkLogoDiv = document.createElement('a');
 linkLogoDiv.href = "#";
@@ -19,7 +20,18 @@ logoDiv.appendChild(linkLogoDiv);
 const imgLogo = document.createElement('img');
 imgLogo.src='../assets/icons/book.svg';
 imgLogo.alt = "logo";
-linkLogoDiv.appendChild(imgLogo)
+linkLogoDiv.appendChild(imgLogo);
+logoDiv.insertAdjacentHTML('afterend', `
+<h1>Lovely Books Shop</h1>
+`);
 
+const main = document.createElement('main');
+const contentMain = document.createElement('div');
+contentMain.classList.add('contentWrapper');
+
+const booksContainer = document.createElement('div');
+contentMain.append(booksContainer);
+main.appendChild(contentMain);
+fragment.append(main);
 body.appendChild(fragment)
 
