7cc398bc71070d21012c89899e4495805e887e83
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-02 01:11:35 +0300

    refactor: delete comments, consoles, change let to const, style replace with variables

--- pages/order/index.js
+++ pages/order/index.js
@@ -18,10 +18,9 @@ function createNewElement(parentSelector, el, attrs, text) {
   if (text) {
     newElement.innerHTML = text;
   }
- parent.append(newElement);
- return newElement;
+  parent.append(newElement);
+  return newElement;
 }
-
 
 const header = document.createElement('header');
 header.className = 'header';
@@ -29,12 +28,12 @@ fragment.prepend(header);
 body.prepend(fragment);
 
 //const header = createNewElement('body', 'header', {class:'header'});
-const contentWrapper = createNewElement('header', 'div', {class:'content-wrapper'});
-const headerWrapper = createNewElement('div.content-wrapper', 'div', {class:'header-wrapper'});
-const logoWrapper = createNewElement('.header-wrapper', 'div', {class:'logo-wrapper'});
-const linkLogo = createNewElement('.logo-wrapper', 'a', {href: "#", class:'link-logo'});
-const imgLogo = createNewElement('.link-logo', 'img', {src:'../../assets/icons/book.svg', alt: 'logo'});
-const titleLogo = createNewElement('.header-wrapper', 'h1', {class:'title-main'}, 'Lovely Books store')
+const contentWrapper = createNewElement('header', 'div', { class: 'content-wrapper' });
+const headerWrapper = createNewElement('div.content-wrapper', 'div', { class: 'header-wrapper' });
+const logoWrapper = createNewElement('.header-wrapper', 'div', { class: 'logo-wrapper' });
+const linkLogo = createNewElement('.logo-wrapper', 'a', { href: "#", class: 'link-logo' });
+const imgLogo = createNewElement('.link-logo', 'img', { src: '../../assets/icons/book.svg', alt: 'logo' });
+const titleLogo = createNewElement('.header-wrapper', 'h1', { class: 'title-main' }, 'Lovely Books store')
 /* 
 const main = document.createElement('main');
 main.className = 'main';
@@ -43,21 +42,17 @@ body.append(fragment) */
 /* const contentMain = createNewElement('main', 'div', {class: 'content-wrapper'});
 const sectionWrapper = createNewElement('main>.content-wrapper', 'div', {class:'flex'});
 const sectionFirst = createNewElement('.content-wrapper>.flex', 'section') */;
-const sectionBag = createNewElement('.content-wrapper.flex','section', {class:'section-bag'});
-const bagWrapper = createNewElement('.section-bag', 'div', {class:'bag-wrapper column'});
-const sectionTitle = createNewElement('.bag-wrapper', 'h2', {class:'bag-title'}, 'Your shopping bag');
-const bagTotalContainer = createNewElement('.bag-wrapper', 'div', {class: 'total-container'});
-const totalText = createNewElement('.total-container', 'span', {class: 'total-text'}, 'Bag Total: $');
-const totalCost = createNewElement('.total-container', 'span', {class: 'total-cost'}, '0');
-const modal = createNewElement('.main', 'div', {class: 'modal hide'});
-const modalDialog = createNewElement('.modal','div',{class: 'modal-dialog'});
-const modalContent = createNewElement('.modal-dialog','div', {class: 'modal-content'});
-const modalTitle = createNewElement('.modal-content', 'h3', {class: 'bag-title'}, 'The order created');
-const closeButton = createNewElement('.modal-content', 'button', {class: 'button close'}, 'Close');
-
-
-
-
+const sectionBag = createNewElement('.content-wrapper.flex', 'section', { class: 'section-bag' });
+const bagWrapper = createNewElement('.section-bag', 'div', { class: 'bag-wrapper column' });
+const sectionTitle = createNewElement('.bag-wrapper', 'h2', { class: 'bag-title' }, 'Your shopping bag');
+const bagTotalContainer = createNewElement('.bag-wrapper', 'div', { class: 'total-container' });
+const totalText = createNewElement('.total-container', 'span', { class: 'total-text' }, 'Bag Total: $');
+const totalCost = createNewElement('.total-container', 'span', { class: 'total-cost' }, '0');
+const modal = createNewElement('.main', 'div', { class: 'modal hide' });
+const modalDialog = createNewElement('.modal', 'div', { class: 'modal-dialog' });
+const modalContent = createNewElement('.modal-dialog', 'div', { class: 'modal-content' });
+const modalTitle = createNewElement('.modal-content', 'h3', { class: 'bag-title' }, 'The order created');
+const closeButton = createNewElement('.modal-content', 'button', { class: 'button close' }, 'Close');
 
 //Form validation
 const form = document.getElementById('form');
@@ -65,118 +60,121 @@ const firstName = document.getElementById('name');
 const surname = document.getElementById('surname');
 const date = document.getElementById('date');
 const street = document.getElementById('street');
-const house =document.getElementById('house');
+const house = document.getElementById('house');
 const flat = document.getElementById('flat');
 const cash = document.getElementById('cash');
 const card = document.getElementById('card');
-const submit =document.querySelector('.submit');
+const submit = document.querySelector('.submit');
 const reset = document.querySelector('.reset');
-
-
 const inputsAll = document.querySelectorAll('input');
+
 inputsAll.forEach(item => {
-  if(item.type != 'submit') {
- item.addEventListener('blur', checkInputs);
- item.addEventListener('input', checkInputs)
+  if (item.type != 'submit') {
+    item.addEventListener('blur', checkInputs);
+    item.addEventListener('input', checkInputs)
   }
-})
-
-  function checkInputs(event) {
-    const firstNameValue = firstName.value.trim();
-    const surnameValue = surname.value.trim();
-    const streetValue = street.value.trim();
-    const houseValue = house.value.trim();
-    const flatValue = flat.value.trim();
-
-    if(event.target.hasAttribute('required') && event.target === firstName) {
-      if((firstNameValue === '') || (firstNameValue.length < 4) || (!(/^[a-zA-Z]+$/i).test(firstNameValue))) {
-        firstName.nextElementSibling.classList.remove('hide')
-        firstName.style.border = '1px solid var(--red)'
-       } else {
-        firstName.nextElementSibling.classList.add('hide');
-        firstName.style.border = '1px solid var(--aqwa)';
-       }
-      }
-      if(event.target.hasAttribute('required') && event.target === surname) {
-        if((surnameValue === '') || (surnameValue.length < 5) || (!(/^[a-zA-Z]+$/i).test(surnameValue))){
-          surname.nextElementSibling.classList.remove('hide')
-          surname.style.border = '1px solid var(--red)'
-        } else {
-          surname.nextElementSibling.classList.add('hide');
-          surname.style.border = '1px solid var(--aqwa)';
-        }
-      }
-      if(event.target.hasAttribute('required') && event.target === street) {
-        if((streetValue === '') || (streetValue.length < 5) || (!(/^[a-zA-Z0-9\s]+$/i).test(streetValue))){
-          street.nextElementSibling.classList.remove('hide')
-          street.style.border = '1px solid var(--red)'
-        } else {
-          street.nextElementSibling.classList.add('hide');
-          street.style.border = '1px solid var(--aqwa)';
-        }
-      }
-      if(event.target.hasAttribute('required') && event.target === house) {
-        if((houseValue === '') || (houseValue < 0) || (!(/^[0-9\s]+$/).test(houseValue))){
-          house.nextElementSibling.classList.remove('hide')
-          house.style.border = '1px solid var(--red)'
-        } else {
-          house.nextElementSibling.classList.add('hide');
-          house.style.border = '1px solid var(--aqwa)';
-        }
-  }
-  if(event.target.hasAttribute('required') && event.target === flat) {
- 
-    if((flatValue === '') || (flatValue < 0) || (!(/^[1-9][0-9-]*$/).test(flatValue))){
-      flat.nextElementSibling.classList.remove('hide')
-      flat.style.border = '1px solid var(--red)'
-    } else {
-      flat.nextElementSibling.classList.add('hide');
-      flat.style.border = '1px solid var(--aqwa)';
-    }
-}
-if(event.target.hasAttribute('required') && event.target === date) {
-  let dateValue = date.value;
-  let today = new Date();
-  let todayYear = today.getFullYear();
-  let todayMonth = today.getMonth()+1;
-  let todayDate = today.getDate();
-  let tomorrow = todayDate +1;
-  if ((tomorrow) < 10) {
-    tomorrow = "0" + tomorrow;
-  }
-  dateValue = todayYear + "-" + todayMonth + "-" + (tomorrow);
-if(date.value < dateValue) {
-date.nextElementSibling.classList.remove('hide')
-date.style.border = '1px solid var(--red)'
-} else {
-date.nextElementSibling.classList.add('hide');
-date.style.border = '1px solid var(--aqwa)';
-}
-}
-}
+});
 
 form.addEventListener("change", () => {
   submit.disabled = !form.checkValidity()
 });
-console.log(modal)
-function showOrderInfo(event){
-  event.preventDefault();
- modal.classList.remove('hide');
-  console.log(modal)
-  modalTitle.insertAdjacentHTML('beforeend', `
-  <p class="modal-descr">Name: ${firstName.value}</p>
-  <p class="modal-descr">Surname: ${surname.value}</p>
-  <p class="modal-descr">Delivery adress: ${street.value} street house ${house.value} flat ${flat.value}</p>
-  <p class="modal-descr">Delivery date: ${date.value}</p>`)
-}
 
 submit.addEventListener('click', showOrderInfo);
 closeButton.addEventListener('click', closeModal);
-function closeModal(){
-  modal.classList.add('hide');
-}
 reset.addEventListener('click', resetForm);
-function resetForm(){
+
+function checkInputs(event) {
+  const firstNameValue = firstName.value.trim();
+  const surnameValue = surname.value.trim();
+  const streetValue = street.value.trim();
+  const houseValue = house.value.trim();
+  const flatValue = flat.value.trim();
+  const red = '1px solid var(--red)';
+  const aqwa = '1px solid var(--aqwa)';
+
+  if (event.target.hasAttribute('required') && event.target === firstName) {
+    if ((firstNameValue === '') || (firstNameValue.length < 4) || (!(/^[a-zA-Z]+$/i).test(firstNameValue))) {
+      firstName.nextElementSibling.classList.remove('hide');
+      firstName.style.border = red;
+    } else {
+      firstName.nextElementSibling.classList.add('hide');
+      firstName.style.border = aqwa;
+    }
+  }
+  if (event.target.hasAttribute('required') && event.target === surname) {
+    if ((surnameValue === '') || (surnameValue.length < 5) || (!(/^[a-zA-Z]+$/i).test(surnameValue))) {
+      surname.nextElementSibling.classList.remove('hide');
+      surname.style.border = red;
+    } else {
+      surname.nextElementSibling.classList.add('hide');
+      surname.style.border = aqwa;
+    }
+  }
+  if (event.target.hasAttribute('required') && event.target === street) {
+    if ((streetValue === '') || (streetValue.length < 5) || (!(/^[a-zA-Z0-9\s]+$/i).test(streetValue))) {
+      street.nextElementSibling.classList.remove('hide');
+      street.style.border = red;
+    } else {
+      street.nextElementSibling.classList.add('hide');
+      street.style.border = aqwa;
+    }
+  }
+  if (event.target.hasAttribute('required') && event.target === house) {
+    if ((houseValue === '') || (houseValue < 0) || (!(/^[0-9\s]+$/).test(houseValue))) {
+      house.nextElementSibling.classList.remove('hide');
+      house.style.border = red;
+    } else {
+      house.nextElementSibling.classList.add('hide');
+      house.style.border = aqwa;
+    }
+  }
+  if (event.target.hasAttribute('required') && event.target === flat) {
+
+    if ((flatValue === '') || (flatValue < 0) || (!(/^[1-9][0-9-]*$/).test(flatValue))) {
+      flat.nextElementSibling.classList.remove('hide');
+      flat.style.border = red;
+    } else {
+      flat.nextElementSibling.classList.add('hide');
+      flat.style.border = aqwa;
+    }
+  }
+  if (event.target.hasAttribute('required') && event.target === date) {
+    let dateValue = date.value;
+    const today = new Date();
+    const todayYear = today.getFullYear();
+    const todayMonth = today.getMonth() + 1;
+    const todayDate = today.getDate();
+    let tomorrow = todayDate + 1;
+    if ((tomorrow) < 10) {
+      tomorrow = "0" + tomorrow;
+    }
+    dateValue = todayYear + "-" + todayMonth + "-" + tomorrow;
+    if (date.value < dateValue) {
+      date.nextElementSibling.classList.remove('hide');
+      date.style.border = red;
+    } else {
+      date.nextElementSibling.classList.add('hide');
+      date.style.border = aqwa;
+    }
+  }
+}
+
+function showOrderInfo(event) {
+  event.preventDefault();
+  modal.classList.remove('hide');
+  modalTitle.insertAdjacentHTML('beforeend', `
+    <p class="modal-descr">Name: ${firstName.value}</p>
+    <p class="modal-descr">Surname: ${surname.value}</p>
+    <p class="modal-descr">Delivery adress: ${street.value} street house ${house.value} flat ${flat.value}</p>
+    <p class="modal-descr">Delivery date: ${date.value}</p>
+  `)
+};
+
+function closeModal() {
+  modal.classList.add('hide');
+};
+
+function resetForm() {
   form.reset();
   submit.disabled = true;
-}
+};
