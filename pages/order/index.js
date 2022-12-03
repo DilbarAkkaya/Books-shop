08defc430705ae7bdbcef6036a22752a9254c6d6
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-04 00:59:30 +0300

    feat: add listener for click close icon

--- pages/order/index.js
+++ pages/order/index.js
@@ -113,7 +113,16 @@ window.addEventListener('load', ()=>{
   });
   
     totalCost.textContent = localStorage.getItem('total');
-})
+});
+bookList.addEventListener('click', (e) => {
+  const card = e.target.closest('.card');
+  if (e.target.closest('.close-icon')) {
+    card.classList.add('hide');
+    const price = card.querySelector('.cost');
+    totalCost.textContent = totalCost.textContent - price.textContent;
+  }
+});
+
   /* const book = `
   <li class="card row">
     <img src=${localStorage.getItem('booksArray')} alt="book image" class="card-img">
